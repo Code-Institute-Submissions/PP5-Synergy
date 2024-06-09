60c79d64d5be095ff755fcc556b66b8163244b82
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-09 10:26:49 +0100

    Add django all auth urls

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
@@ -38,6 +38,7 @@ DEBUG = 'DEV' in os.environ
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
    'localhost',
+   '127.0.0.1',
    ]
 
 
