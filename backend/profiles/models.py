0c2ce30c3a08d8a1f82389e61a91643992ab743d
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-09 10:53:16 +0100

    Add profile model

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    avatar = models.ImageField(
+        upload_to='images/avatar/', default='../default_profile_dkfqgb'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)
