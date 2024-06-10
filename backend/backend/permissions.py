f2741b651b3eb0646d6417ae74c89a5258f9671c
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-10 17:19:57 +0100

    Add permission for staff user

--- backend/backend/permissions.py
+++ backend/backend/permissions.py
@@ -13,3 +13,12 @@ class IsWorkstreamOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.workstream.owner == request.user
+
+
+class IsStaffOrReadOnly(permissions.BasePermission):
+    print('start')
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        print(request.user.user_participant.get(workstream=request.user.profile.default_workstream).is_staff)
+        return request.user.user_participant.get(workstream=request.user.profile.default_workstream).is_staff
