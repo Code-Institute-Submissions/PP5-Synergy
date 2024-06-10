c49f20371fb90fc120fec1651deb4c9584013e7d
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-10 12:12:40 +0100

    Add backend profile app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for backend project.
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,17 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-    # path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    # our logout route has to be above the default one to be matched first
-    # path('dj-rest-auth/logout/', logout_route),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path(
-        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
-    ),
     path('api/', include('profiles.urls')),
-    # path("", include("frontend.urls")),
 ]
