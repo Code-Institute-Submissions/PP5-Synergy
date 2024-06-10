c1a56170fc3306749b15a5a0b753ddc10a2e9bdc
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-10 17:19:11 +0100

    Add category view

--- backend/category/views.py
+++ backend/category/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics
+from backend.permissions import IsStaffOrReadOnly
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryList(generics.ListCreateAPIView):
+    """
+    List posts or create a post if logged in
+    The perform_create method associates the post with the logged in user.
+    """
+    serializer_class = CategorySerializer
+    permission_classes = [IsStaffOrReadOnly]
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        return Category.objects.filter(workstream=user.profile.default_workstream)
+    
+    def perform_create(self, serializer):
+        serializer.save(
+            owner=self.request.user,
+            workstream=self.request.user.profile.default_workstream
+            )
+
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it.
+    """
+    serializer_class = CategorySerializer
+    permission_classes = [IsStaffOrReadOnly]
+    queryset = Category.objects.all().order_by('-created_at')
