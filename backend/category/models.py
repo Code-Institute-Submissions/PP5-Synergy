6c098de771d5611f568a6c57154a7c2f8c154e0a
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-10 14:30:52 +0100

    Add category model

--- backend/category/models.py
+++ backend/category/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from workstream.models import Workstream
 
-# Create your models here.
+
+class Category(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    workstream = models.ForeignKey(Workstream, on_delete=models.CASCADE, related_name='category_workstream')
+
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
