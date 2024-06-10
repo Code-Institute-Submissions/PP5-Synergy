a84b407e9ec23fdc43fec586a75653a25a207f7a
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-10 14:08:58 +0100

    Add participant serializer

--- backend/workstream/serializers.py
+++ backend/workstream/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Workstream
+from .models import Workstream, Participant
 
 
 class WorkstreamSerializer(serializers.ModelSerializer):
@@ -16,4 +16,21 @@ class WorkstreamSerializer(serializers.ModelSerializer):
         model = Workstream
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name', 'users', 'is_owner',
+        ]
+
+class ParticipantSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='user.username')
+    workstream = serializers.ReadOnlyField(source='workstream.name')
+    is_owner = serializers.SerializerMethodField()
+    
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.workstream.owner
+
+
+    class Meta:
+        model = Participant
+        fields = [
+            'id', 'owner', 'workstream', 'is_staff', 'is_owner',
         ]
