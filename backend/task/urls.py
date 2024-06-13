02e9401c672092334400ea7bb8803d08d9c1870a
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-13 12:28:45 +0100

    Add task assign urls

--- backend/task/urls.py
+++ backend/task/urls.py
@@ -4,4 +4,6 @@ from task import views
 urlpatterns = [
     path('task/', views.TaskList.as_view()),
     path('task/<int:pk>/', views.TaskDetail.as_view()),
+    path('task/<int:pk>/admin', views.TaskAssignAdmin.as_view()),
+    path('task/<int:pk>/assign', views.TaskAssignSelf.as_view()),
 ]
