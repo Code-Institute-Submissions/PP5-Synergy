6d261b47fd81e22ecd83b1974ff993538df09df9
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-13 11:51:16 +0100

    Add task serializer

---
+++ backend/task/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Task
+from category.models import Category
+
+
+class CategoryForeignKey(serializers.PrimaryKeyRelatedField):
+    def get_queryset(self):
+        user = self.context['request'].user
+        return Category.objects.filter(workstream=user.profile.default_workstream)
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='user.username')
+    is_owner = serializers.SerializerMethodField()
+    category = CategoryForeignKey()
+    
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+
+    class Meta:
+        model = Task
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'name', 'detail',
+            'is_owner', 'priority', 'is_completed', 'category', 'author',
+            'deadline'
+        ]
