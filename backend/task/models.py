72d4f53de46ab596604ce9715ea86101e235e75d
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-13 11:52:17 +0100

    Add task models

---
+++ backend/task/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from category.models import Category
+
+
+class Task(models.Model):
+    PRIORITY_LEVELS = [
+        (1, 'No-priority'),
+        (2, 'Low-priority'),
+        (3, 'Medium-priority'),
+        (4, 'High-priority'),
+    ]
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    detail = models.TextField(blank=True)
+    priority = models.IntegerField(choices=PRIORITY_LEVELS, default=1)
+    is_completed = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name='user')
+    deadline = models.DateTimeField()
+
+
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
