b942f1ae7771661ec51819e1b5d44b4f602c258a
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-13 14:32:36 +0100

    Add project model

---
+++ backend/project/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from workstream.models import Workstream
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    workstream = models.ForeignKey(Workstream, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
