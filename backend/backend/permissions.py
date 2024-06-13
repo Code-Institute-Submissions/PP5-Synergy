181131e7f4e22085c18fde7feda83d84c345cb60
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-13 12:29:44 +0100

    Add permission for task author

--- backend/backend/permissions.py
+++ backend/backend/permissions.py
@@ -8,6 +8,12 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         return obj.owner == request.user
 
 
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
 class IsWorkstreamOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
