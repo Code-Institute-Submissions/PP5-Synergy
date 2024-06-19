5234d5380bd0089f379dea901e7c648cf6ff6a12
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-19 10:05:50 +0100

    Add join request and accept serializer

--- backend/invite/serializers.py
+++ backend/invite/serializers.py
@@ -1,9 +1,46 @@
 from rest_framework import serializers
 from .models import Invite
+from workstream.models import Workstream
 
 
 class InviteSerializer(serializers.ModelSerializer):
+    accepted = serializers.ReadOnlyField()
+    inbound = serializers.ReadOnlyField()
     workstream = serializers.ReadOnlyField(source='workstream.name')
+
+
+    class Meta:
+        model = Invite
+        fields = [
+            'id', 'created_at', 'created_at', 'workstream', 'user',
+            'accepted', 'inbound',
+        ]
+
+
+class WorkstreamForeignKey(serializers.PrimaryKeyRelatedField):
+    def get_queryset(self):
+        user = self.context['request'].user
+        return Workstream.objects.exclude(ws_participants__owner=user)
+
+class JoinSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    accepted = serializers.ReadOnlyField()
+    inbound = serializers.ReadOnlyField()
+    workstream = WorkstreamForeignKey()
+
+
+    class Meta:
+        model = Invite
+        fields = [
+            'id', 'created_at', 'created_at', 'workstream', 'user',
+            'accepted', 'inbound',
+        ]
+
+class AcceptSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    workstream = serializers.ReadOnlyField(source='workstream.name')
+    inbound = serializers.ReadOnlyField()
+
 
 
     class Meta:
