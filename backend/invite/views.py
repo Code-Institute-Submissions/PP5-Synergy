4cad4eba8484ab0937df157c281a92474a656328
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-19 10:05:23 +0100

    Add join request views

--- backend/invite/views.py
+++ backend/invite/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
-from backend.permissions import IsWorkstreamOwnerOrReadOnly
+from backend.permissions import IsWorkstreamOwnerOrReadOnly, IsInviteOwnerOrReadOnly
 from .models import Invite
-from .serializers import InviteSerializer
+from .serializers import InviteSerializer, JoinSerializer, AcceptSerializer
 
 
 class InviteList(generics.ListCreateAPIView):
@@ -31,6 +31,38 @@ class InviteDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve a post and edit or delete it if you own it.
     """
-    serializer_class = InviteSerializer
+    serializer_class = AcceptSerializer
     permission_classes = [IsWorkstreamOwnerOrReadOnly]
+    queryset = Invite.objects.all().order_by('-created_at')
+
+
+class JoinList(generics.ListCreateAPIView):
+    """
+    List posts or create a post if logged in
+    The perform_create method associates the post with the logged in user.
+    """
+    serializer_class = JoinSerializer
+    permission_classes = [IsInviteOwnerOrReadOnly]
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        return Invite.objects.filter(user=user)
+    
+    def perform_create(self, serializer):
+        serializer.save(
+            user=self.request.user,
+            inbound=True
+            )
+
+
+class JoinDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it.
+    """
+    serializer_class = AcceptSerializer
+    permission_classes = [IsInviteOwnerOrReadOnly]
     queryset = Invite.objects.all().order_by('-created_at')
