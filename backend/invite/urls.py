78e0210839fdbccbaa4255c087e1c0969cd6ef34
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-19 10:05:06 +0100

    Add join request url

--- backend/invite/urls.py
+++ backend/invite/urls.py
@@ -4,4 +4,6 @@ from invite import views
 urlpatterns = [
     path('invite/', views.InviteList.as_view()),
     path('invite/<int:pk>/', views.InviteDetail.as_view()),
+    path('join/', views.JoinList.as_view()),
+    path('join/<int:pk>/', views.JoinDetail.as_view()),
 ]
