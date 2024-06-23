d84d722aee35c595ca65f94071614b6dab826633
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-23 11:14:31 +0100

    Add primereact menubar component

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Menubar } from 'primereact/menubar';
+import { Button } from 'primereact/button';
+
+const NavBar = () => {
+
+    const items = [
+        {
+            label: 'Features',
+            icon: 'pi pi-star',
+            url: '/'
+        }
+    ];
+
+    const end = (
+        <div className="flex align-items-center gap-2">
+            <Button label="Get Started" size="small" />
+        </div>
+    );
+
+    const start = <a href='/'><img alt="logo" src="https://primefaces.org/cdn/primereact/images/logo.png" height="40" className="mr-2"></img></a>;
+
+    return (
+        <div className="card">
+            <Menubar model={items} start={start} end={end} />
+        </div>
+    )
+}
+
+export default NavBar
