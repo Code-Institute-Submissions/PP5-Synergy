2341b490fe92122e962ef7870bab742f53905fcb
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-23 12:31:40 +0100

    Add Router component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,12 +8,15 @@ import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <PrimeReactProvider>
-    <App />
+      <Router>
+        <App />
+      </Router>
     </PrimeReactProvider>
   </React.StrictMode>
 );
