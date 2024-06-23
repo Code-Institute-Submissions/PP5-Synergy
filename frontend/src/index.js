8a926f38713b0b1123917b1a8cc17a782366b6fb
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-23 11:36:06 +0100

    Add primeflex css import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { PrimeReactProvider } from 'primereact/api';
+import 'primeflex/primeflex.css';  
 import 'primereact/resources/primereact.css';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
