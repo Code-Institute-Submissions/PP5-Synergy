63ce536644b1b5e7cc892d7376c3c25edc360bac
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-23 12:31:08 +0100

    Add routes to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <div>
+        <Switch>
+          <Route exact path="/" render={() => <h2>Home</h2>} />
+        </Switch>
+        <Switch>
+          <Route exact path="/signup" render={() => <h2>Signup</h2>} />
+        </Switch>
+      </div>
     </div>
   );
 }
