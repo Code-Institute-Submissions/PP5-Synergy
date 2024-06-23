7a7ba6586b06dea25d38639982f869dce4014cdf
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-23 11:13:55 +0100

    Add Navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import { Button } from 'primereact/button';
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Button label="Submit" />
+      <NavBar />
     </div>
   );
 }
