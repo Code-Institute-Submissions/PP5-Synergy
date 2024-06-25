8653e48845c19649641a630177ed3efe1efe8723
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-25 10:06:34 +0100

    Add Sign in component to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Route, Routes } from 'react-router-dom/';
+import './api/axiosDefaults';
+import SignIpForm from './pages/auth/SignInForm'
 
 
 function App() {
@@ -9,7 +11,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route exact path="/" element={<h2>Home</h2>} />
-        <Route exact path="/signin" element={<h2>Sign In</h2>} />
+        <Route exact path="/signin" element={<SignIpForm />} />
         <Route exact path="/signup" element={<h2>Sign Up</h2>} />
         <Route render={() => <p>Page not found!</p>} />
       </Routes>
