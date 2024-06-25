f347d333a33601e8a84de0f4d07aae80f8d5878d
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-25 18:50:55 +0100

    Add handle iput change and form submit

--- frontend/src/pages/auth/SignInForm.js
+++ frontend/src/pages/auth/SignInForm.js
@@ -7,37 +7,54 @@ import { Password } from 'primereact/password';
 
 const LoginPage = () => {
     const [checked, setChecked] = useState(false);
-    const [value, setValue] = useState('');
+    const [signInData, setSignInData] = useState({
+        username: "",
+        password: "",
+    });
+    const { username, password } = signInData;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('form submitted');
+    };
+
+    const handleChange = (event) => {
+        setSignInData({
+          ...signInData,
+          [event.target.name]: event.target.value,
+        });
+    };
 
     return (
         
     <div className="flex align-items-center justify-content-center mt-2">
-        <div className="surface-card p-4 shadow-2 border-round w-full lg:w-4">
+        <div className="surface-card p-4 shadow-2 border-round w-full sm:w-6 lg:w-4">
             <div className="text-center mb-5">
                 <img src="/demo/images/blocks/logos/hyper.svg" alt="hyper" height={50} className="mb-3" />
                 <div className="text-900 text-3xl font-medium mb-3">Welcome Back</div>
                 <span className="text-600 font-medium line-height-3">Don't have an account?</span>
-                <a className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</a>
+                <Link className="font-medium no-underline ml-2 text-blue-500 cursor-pointer" to="/signup">
+                    Create today!
+                </Link>
             </div>
 
-            <div>
-                <label htmlFor="email" className="block text-900 font-medium mb-2">Email</label>
-                <InputText id="email" type="text" placeholder="Email address" className="w-full mb-3" />
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username" className="block text-900 font-medium mb-2">Username</label>
+                <InputText value={username} onChange={handleChange} id="username" name="username" type="text" placeholder="username" className="w-full mb-3" />
 
                 <label htmlFor="password" className="block text-900 font-medium mb-2">Password</label>
-                <InputText id="password" type="password" placeholder="Password" className="w-full mb-3" />
-                <Password value={value} onChange={(e) => setValue(e.target.value)} className="w-12 mb-0"/>
+                <Password value={password} onChange={handleChange} className="w-12 mb-3" pt={{ input: { className: "w-12", name: "password" }}}/>
 
                 <div className="flex align-items-center justify-content-between mb-6">
                     <div className="flex align-items-center">
                         <Checkbox id="rememberme" onChange={e => setChecked(e.checked)} checked={checked} className="mr-2" />
                         <label htmlFor="rememberme">Remember me</label>
                     </div>
-                    <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot your password?</a>
+                    <span className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot your password?</span>
                 </div>
 
                 <Button label="Sign In" icon="pi pi-user" className="w-full" />
-            </div>
+            </form>
         </div>
     </div>
     );
