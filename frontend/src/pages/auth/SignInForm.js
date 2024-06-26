98429499d3b8c691e70ec9dd5d37e3224c0c18c8
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-26 19:16:38 +0100

    Add form validation message

--- frontend/src/pages/auth/SignInForm.js
+++ frontend/src/pages/auth/SignInForm.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 import axios from "axios";
-import React, { useContext, useState, useRef } from 'react';
+import React, { useContext, useState } from 'react';
 import { Checkbox } from 'primereact/checkbox';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
-import { Toast } from 'primereact/toast';
+import { Message } from 'primereact/message';
 
 const LoginPage = () => {
     const [checked, setChecked] = useState(false);
@@ -15,18 +15,14 @@ const LoginPage = () => {
     });
     const { username, password } = signInData;
     const [errors, setErrors] = useState({});
-
-    const toast = useRef(null);
     
     const handleSubmit = async (event) => {
         event.preventDefault();
     
         try {
           const { data } = await axios.post("/dj-rest-auth/login/", signInData);
-          console.log(data)
         } catch (err) {
             setErrors(err.response?.data);
-            toast.current.show({ severity: 'info', summary: 'Info', detail: 'Incorrect Details Entered' })
         }
     };
 
@@ -41,7 +37,6 @@ const LoginPage = () => {
         
     <div className="flex align-items-center justify-content-center mt-2">
         <div className="surface-card p-4 shadow-2 border-round w-full sm:w-6 lg:w-4">
-            <Toast ref={toast} />
             <div className="text-center mb-5">
                 <img src="/demo/images/blocks/logos/hyper.svg" alt="hyper" height={50} className="mb-3" />
                 <div className="text-900 text-3xl font-medium mb-3">Welcome Back</div>
@@ -49,14 +44,24 @@ const LoginPage = () => {
                 <Link className="font-medium no-underline ml-2 text-blue-500 cursor-pointer" to="/signup">
                     Create today!
                 </Link>
+                {errors.non_field_errors?.map((message, idx) => (
+                    <Message className="w-full mb-1" severity="error" text={message} />
+                ))}
             </div>
 
             <form onSubmit={handleSubmit}>
                 <label htmlFor="username" className="block text-900 font-medium mb-2">Username</label>
                 <InputText value={username} onChange={handleChange} id="username" name="username" type="text" placeholder="username" className="w-full mb-3" />
+                {errors.username?.map((message, idx) => (
+                    <Message className="w-full mb-1" severity="error" text={message} />
+                ))}
+                
 
                 <label htmlFor="password" className="block text-900 font-medium mb-2">Password</label>
                 <Password value={password} onChange={handleChange} className="w-12 mb-3" feedback={false} pt={{ input: { className: "w-12", name: "password" }}}/>
+                {errors.password?.map((message, idx) => (
+                    <Message className="w-full mb-1" severity="error" text={message} />
+                ))}
 
                 <div className="flex align-items-center justify-content-between mb-6">
                     <div className="flex align-items-center">
