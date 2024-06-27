dfe48db96b4d7f96b79a4f8b64b599b2accec74d
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-27 19:52:13 +0100

    Add custom context provide to app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,15 +9,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { CurrentUserProvider } from './contexts/CurrentUserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
     <PrimeReactProvider>
-      
+      <CurrentUserProvider>
         <App />
-      
+      </CurrentUserProvider>
     </PrimeReactProvider>
     </Router>
   </React.StrictMode>
