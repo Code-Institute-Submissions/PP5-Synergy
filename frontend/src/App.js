c778f6f14cab6572c334dd13a113c68cde72c1a8
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-27 19:49:53 +0100

    Add current user context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,16 +7,17 @@ import SignUpForm from './pages/auth/SignUpForm'
 
 
 function App() {
+
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<h2>Home</h2>} />
-        <Route exact path="/signin" element={<SignIpForm />} />
-        <Route exact path="/signup" element={<SignUpForm />} />
-        <Route render={() => <p>Page not found!</p>} />
-      </Routes>
-    </div>
+        <div className={styles.App}>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<h2>Home</h2>} />
+            <Route exact path="/signin" element={<SignIpForm />} />
+            <Route exact path="/signup" element={<SignUpForm />} />
+            <Route render={() => <p>Page not found!</p>} />
+          </Routes>
+        </div>
   );
 }
 
