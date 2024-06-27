fc07d0a29b826824fd04d45c996f3b6b8b13c1c3
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-27 09:46:38 +0100

    Add redirect after user creation

--- frontend/src/pages/auth/SignUpForm.js
+++ frontend/src/pages/auth/SignUpForm.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import React, { useContext, useState } from 'react';
 import { Button } from 'primereact/button';
@@ -14,6 +14,7 @@ const SignUpForm = () => {
     });
     const { username, password1, password2 } = signUpData;
     const [errors, setErrors] = useState({});
+    const navigate = useNavigate();
     
     const handleChange = (event) => {
         setSignUpData({
@@ -26,6 +27,7 @@ const SignUpForm = () => {
         event.preventDefault();
         try {
             await axios.post("/dj-rest-auth/registration/", signUpData);
+            navigate('/signin')
         } catch (err) {
             setErrors(err.response?.data);
         }
@@ -46,29 +48,26 @@ const SignUpForm = () => {
                     <Message className="w-full mb-1" severity="error" text={message} />
                 ))}
             </div>
+            <label htmlFor="username" className="block text-900 font-medium mb-2">Username</label>
+            <InputText value={username} onChange={handleChange} id="username" name="username" type="text" placeholder="username" className="w-full mb-3" />
+            {errors.username?.map((message, idx) => (
+                <Message className="w-full mb-1" severity="error" text={message} />
+            ))}
+            
 
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="username" className="block text-900 font-medium mb-2">Username</label>
-                <InputText value={username} onChange={handleChange} id="username" name="username" type="text" placeholder="username" className="w-full mb-3" />
-                {errors.username?.map((message, idx) => (
-                    <Message className="w-full mb-1" severity="error" text={message} />
-                ))}
-                
+            <label htmlFor="password" className="block text-900 font-medium mb-2">Password</label>
+            <Password value={password1} onChange={handleChange} className="w-12 mb-3" feedback={false} pt={{ iconField: { root: { className: "w-12" } },input: { className: "w-12", name: "password1" }}} toggleMask/>
+            {errors.password1?.map((message, idx) => (
+                <Message className="w-full mb-1" severity="error" text={message} />
+            ))}
 
-                <label htmlFor="password" className="block text-900 font-medium mb-2">Password</label>
-                <Password value={password1} onChange={handleChange} className="w-12 mb-3" feedback={false} pt={{ iconField: { root: { className: "w-12" } },input: { className: "w-12", name: "password1" }}} toggleMask/>
-                {errors.password1?.map((message, idx) => (
-                    <Message className="w-full mb-1" severity="error" text={message} />
-                ))}
+            <label htmlFor="password2" className="block text-900 font-medium mb-2">Confirm Password</label>
+            <Password value={password2} onChange={handleChange} className="w-12 mb-3" feedback={false} pt={{ iconField: { root: { className: "w-12" } },input: { className: "w-12", name: "password2" }}} toggleMask/>
+            {errors.password2?.map((message, idx) => (
+                <Message className="w-full mb-1" severity="error" text={message} />
+            ))}
 
-                <label htmlFor="password2" className="block text-900 font-medium mb-2">Confirm Password</label>
-                <Password value={password2} onChange={handleChange} className="w-12 mb-3" feedback={false} pt={{ iconField: { root: { className: "w-12" } },input: { className: "w-12", name: "password2" }}} toggleMask/>
-                {errors.password2?.map((message, idx) => (
-                    <Message className="w-full mb-1" severity="error" text={message} />
-                ))}
-
-                <Button label="Submit" icon="pi pi-check" className="w-full" />
-            </form>
+            <Button label="Submit" icon="pi pi-check" className="w-full" onClick={handleSubmit}/>
         </div>
     </div>
     );
