8fb36ded1922fc0b958176967a221be9466ba521
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-27 10:02:53 +0100

    Add fix key errors and label error

--- frontend/src/pages/auth/SignInForm.js
+++ frontend/src/pages/auth/SignInForm.js
@@ -45,20 +45,20 @@ const SignInForm = () => {
                     Create today!
                 </Link>
                 {errors.non_field_errors?.map((message, idx) => (
-                    <Message className="w-full mb-1" severity="error" text={message} />
+                    <Message className="w-full mb-1" severity="error" text={message} key={idx}/>
                 ))}
             </div>
             <label htmlFor="username" className="block text-900 font-medium mb-2">Username</label>
             <InputText value={username} onChange={handleChange} id="username" name="username" type="text" placeholder="username" className="w-full mb-3" />
             {errors.username?.map((message, idx) => (
-                <Message className="w-full mb-1" severity="error" text={message} />
+                <Message className="w-full mb-1" severity="error" text={message} key={idx}/>
             ))}
             
 
             <label htmlFor="password" className="block text-900 font-medium mb-2">Password</label>
-            <Password value={password} onChange={handleChange} className="w-12 mb-3" feedback={false} pt={{ input: { className: "w-12", name: "password" }}}/>
+            <Password value={password} onChange={handleChange} className="w-12 mb-3" feedback={false} pt={{ input: { className: "w-12", name: "password", id: "password" }}}/>
             {errors.password?.map((message, idx) => (
-                <Message className="w-full mb-1" severity="error" text={message} />
+                <Message className="w-full mb-1" severity="error" text={message} key={idx}/>
             ))}
 
             <div className="flex align-items-center justify-content-between mb-6">
