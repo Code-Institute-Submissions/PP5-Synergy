6353ced293fa9b23228df64ef766d5e814a62a82
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-28 21:35:42 +0100

    Add profile avatar and workstream to user serializer

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -4,9 +4,10 @@ from rest_framework import serializers
 
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    profile_avatar = serializers.ReadOnlyField(source='profile.image.url')
+    default_workstream = serializers.ReadOnlyField(source='profile.default_workstream')
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image'
+            'profile_id', 'profile_avatar', 'default_workstream'
         )
