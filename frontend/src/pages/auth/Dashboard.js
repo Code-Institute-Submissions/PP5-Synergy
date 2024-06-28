771678ee59bc172fa4b3fe497a09c5859149e990
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-28 10:00:21 +0100

    Add Dashboard component

--- frontend/src/pages/auth/Dashboard.js
+++ frontend/src/pages/auth/Dashboard.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import DashMenu from '../../components/DashMenu'
 
 function Dashboard() {
   return (
-    <div>
-      Dashboard
+    <div className='grid grid-nogutter'>
+      <div className='hidden sm:block col-fixed p-0 md:w-15rem'>
+        <DashMenu />
+      </div>
+      <div class="col surface-ground p-0">
+        Content
+      </div>
     </div>
   )
 }
