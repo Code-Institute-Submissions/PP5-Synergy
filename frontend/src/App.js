63a354cf5d62ee430f30368ad857c1f8041c110f
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-30 10:24:29 +0100

    Add Dashmenu component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import './api/axiosDefaults';
 import SignIpForm from './pages/auth/SignInForm'
 import SignUpForm from './pages/auth/SignUpForm'
 import Dashboard from './pages/auth/Dashboard';
+import DashMenu from './components/DashMenu';
 
 
 function App() {
@@ -12,13 +13,19 @@ function App() {
   return (
         <div className={styles.App}>
           <NavBar />
-          <Routes>
-            <Route exact path="/" element={<h2>Home</h2>} />
-            <Route exact path="/signin" element={<SignIpForm />} />
-            <Route exact path="/signup" element={<SignUpForm />} />
-            <Route exact path="/dashboard" element={<Dashboard />} />
-            <Route path="*" element={<h2>Page not found</h2>} />
-          </Routes>
+          <div className='grid grid-nogutter'>
+              <DashMenu />
+            <div className="col surface-ground p-0">
+            <Routes>
+              <Route exact path="/" element={<h2>Home</h2>} />
+              <Route exact path="/signin" element={<SignIpForm />} />
+              <Route exact path="/signup" element={<SignUpForm />} />
+              <Route exact path="/dashboard" element={<Dashboard />} />
+              <Route path="*" element={<h2>Page not found</h2>} />
+            </Routes>
+            </div>
+          </div>
+          
         </div>
   );
 }
