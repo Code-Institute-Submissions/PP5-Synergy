cfd27b96b4f08f06bb0a4891d2a59391a79ae192
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-06-30 10:23:59 +0100

    Add remove dashmenu component

--- frontend/src/pages/auth/Dashboard.js
+++ frontend/src/pages/auth/Dashboard.js
@@ -1,16 +1,8 @@
 import React from 'react'
-import DashMenu from '../../components/DashMenu'
 
 const Dashboard = () => {
   return (
-    <div className='grid grid-nogutter'>
-      <div className='hidden sm:block col-fixed p-0 md:w-15rem'>
-        <DashMenu />
-      </div>
-      <div className="col surface-ground p-0">
-        Content
-      </div>
-    </div>
+    <h2>Dashboard</h2>
   )
 }
 
