8133caea793aa3af292c33dadf46a5330f0c4e92
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-07-02 09:45:27 +0100

    Add workstream users details

--- frontend/src/pages/workstream/ActiveWorkstream.js
+++ frontend/src/pages/workstream/ActiveWorkstream.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { axiosReq } from '../../api/axiosDefaults'
+import { Fieldset } from 'primereact/fieldset';
+import { Avatar } from 'primereact/avatar';
+import { AvatarGroup } from 'primereact/avatargroup';
 
 const ActiveWorkstream = () => {
     const { id }  = useParams()
-    const [workstream , setWorkstream] = useState({ results: [] })
+    const [workstream , setWorkstream] = useState({results: []})
     const [category, setCategory] = useState({results: []})
     const [project, setProject] = useState({results: []})
     const [task, setTask] = useState({results: []})
@@ -16,7 +20,10 @@ const ActiveWorkstream = () => {
               axiosReq.get(`/api/workstream/${id}`),
             ]);
             setWorkstream({ results: [workstream] });
+            console.log(workstream)
           } catch (err) {
+            setErrors(err.response?.data);
+            console.log(errors)
           }
         };
     
@@ -24,9 +31,37 @@ const ActiveWorkstream = () => {
     }, [id]);
     
     return (
-        <div>
-        <p>{id}</p>
-        </div>
+        <>
+        { workstream.results.length ? (
+            workstream.results.map((object, idx) => (
+                <Fieldset key={idx} legend={object.name}>
+                <h4>Author:<span className='text-color-secondary px-2'>{object.owner.username}</span></h4>
+                <div>
+                    <h4>Users:</h4>
+                    <div className="card flex justify-content-start">
+                        <AvatarGroup>
+                            {object.users?.map((user, idx) => (
+                                <Avatar image={user?.profile_avatar} size="large" shape="circle" />
+                            ))}
+                            <Avatar label="+" shape="circle" size="large"/>
+                        </AvatarGroup>
+                    </div>
+                </div>
+                <div>
+                    <h4>Categories:</h4>
+                </div>
+                <div>
+                    <h4>Projects:</h4>
+                </div>
+                <div>
+                    <h4>Tasks:</h4>
+                </div>
+            </Fieldset>
+              ))
+        ) : null
+        }
+        
+        </>
     )
 }
 
