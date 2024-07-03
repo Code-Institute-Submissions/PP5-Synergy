279341f1b5439e84ea2598820582c5800cd8961e
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-07-03 10:13:03 +0100

    Add user specific participants list url

--- backend/workstream/urls.py
+++ backend/workstream/urls.py
@@ -3,6 +3,7 @@ from workstream import views
 
 urlpatterns = [
     path('workstream/', views.WorkstreamList.as_view()),
+    path('workstream/active/', views.ParticipantListActive.as_view()),
     path('workstream/participant/', views.ParticipantList.as_view()),
     path('workstream/<int:pk>', views.WorkstreamDetail.as_view()),
     path('workstream/participant/<int:pk>', views.ParticipantDetail.as_view()),
