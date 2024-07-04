9e9f26df16c299d91d068f72231b3391463384a8
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-07-04 20:38:16 +0100

    Add allauth

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,9 +78,9 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'dj_rest_auth',
     'django.contrib.sites',
-    # 'allauth',
-    # 'allauth.account',
-    # 'allauth.socialaccount',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'dj_rest_auth.registration',
     'corsheaders',
 
