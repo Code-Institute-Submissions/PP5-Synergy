cd830c1090e13acf3a10d13e8ad4a86b5ab9fd57
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-07-04 10:19:21 +0100

    Add option button for task

--- frontend/src/components/WorkstreamTask.js
+++ frontend/src/components/WorkstreamTask.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Chip } from 'primereact/chip';
 import { Checkbox } from "primereact/checkbox";
+import { SpeedDial } from 'primereact/speeddial';
 
 const WorkstreamTask = (props) => {
     const {
@@ -18,6 +19,32 @@ const WorkstreamTask = (props) => {
     } = props
 
     const [isCompleted, setIsCompleted] = useState(is_completed)
+
+    const items = [
+        {
+            label: 'Add',
+            icon: 'pi pi-pencil',
+        },
+        {
+            label: 'Update',
+            icon: 'pi pi-refresh',
+        },
+        {
+            label: 'Delete',
+            icon: 'pi pi-trash',
+        },
+        {
+            label: 'Upload',
+            icon: 'pi pi-upload',
+        },
+        {
+            label: 'React Website',
+            icon: 'pi pi-external-link',
+            command: () => {
+                window.location.href = 'https://react.dev/';
+            }
+        }
+    ];
     
     const handleCheckbox = async (e) => {
         e.preventDefault();
@@ -28,8 +55,11 @@ const WorkstreamTask = (props) => {
 
     return (
         <div className="card flex justify-content-center">
-            {owner && (<Checkbox onChange={handleCheckbox} checked={isCompleted}></Checkbox>)}
             <Chip label={name} image={owner?.profile_avatar}/>
+            <span className='pi pi-clock'></span>
+            <span>{deadline}</span>
+            {owner && (<Checkbox onChange={handleCheckbox} checked={isCompleted}></Checkbox>)}
+            <SpeedDial className='relative' model={items} radius={80} type="semi-circle" direction="left" transitionDelay={80} showIcon="pi pi-ellipsis-v" hideIcon="pi pi-times" buttonClassName="p-button-text w-2rem h-1rem"/>
         </div>
         
     )
