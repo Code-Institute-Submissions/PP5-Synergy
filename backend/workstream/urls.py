ad238766b0db1df565f20961e047cd166968cd1a
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-07-07 15:07:43 +0100

    Add updated urls for workstreams app

--- backend/workstream/urls.py
+++ backend/workstream/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('workstream/', views.WorkstreamList.as_view()),
     path('workstream/active/', views.ParticipantListActive.as_view()),
     path('workstream/participant/', views.ParticipantList.as_view()),
-    path('workstream/<int:pk>', views.WorkstreamDetail.as_view()),
-    path('workstream/participant/<int:pk>', views.ParticipantDetail.as_view()),
+    path('workstream/<int:pk>/', views.WorkstreamDetail.as_view()),
+    path('workstream/participant/<int:pk>/', views.ParticipantDetail.as_view()),
 ]
