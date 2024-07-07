c7091da42d0ff8441ff28d36f31580722bc7c547
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-07-07 16:12:12 +0100

    Add is_owner extra field to category serializer

--- backend/category/serializers.py
+++ backend/category/serializers.py
@@ -5,10 +5,15 @@ from .models import Category
 class CategorySerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     workstream = serializers.ReadOnlyField(source='workstream.name')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
 
     class Meta:
         model = Category
         fields = [
-            'id', 'owner', 'name', 'workstream'
+            'id', 'owner', 'name', 'workstream', 'is_owner',
         ]
