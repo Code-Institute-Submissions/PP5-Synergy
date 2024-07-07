385c0aca0aaaf74a05da7dc2d2c8e174d45cf26e
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-07-07 10:52:15 +0100

    Add switch workstream functionality

--- frontend/src/pages/workstream/WorkstreamList.js
+++ frontend/src/pages/workstream/WorkstreamList.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Card } from 'primereact/card';
-import { Avatar } from 'primereact/avatar';
-import { AvatarGroup } from 'primereact/avatargroup';
+import { Toast } from 'primereact/toast';
 import { useNavigate } from 'react-router-dom';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { axiosReq } from '../../api/axiosDefaults';
 
 
 const Workstream = (props) => {
+    const currentUser = useCurrentUser()
+    const [errors, setErrors] = useState({});
+    const toast = useRef(null);
+    
     const navigate = useNavigate()
     const {
         id,
@@ -16,10 +21,44 @@ const Workstream = (props) => {
         users,
         is_owner,
     } = props;
+    const inputData = {default_workstream: id}
 
+    const showError = () => {
+        toast.current.show({
+            severity:'error', 
+            summary: 'Unauthorized Access', 
+            content: (props) => (
+                <div className="flex flex-column align-items-left" style={{ flex: '1' }}>
+                    <div className="font-medium text-lg my-3 text-900">{props.message.summary}</div>
+                    <div className="flex align-items-center gap-2">
+                        <span className="font-bold text-900">You are not participating in "{name}"</span>
+                    </div>
+                    
+                </div>
+            ),
+            life: 3000
+        });
+    }
+
+    const handleSwitch = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await axiosReq.put(`api/profiles/switch/${currentUser.pk}/`, inputData);
+            console.log(data)
+            navigate('/workstream/active')
+        } catch (err) {
+            setErrors(err.response?.data.default_workstream[0]);
+            showError()
+            console.log(errors)
+        }
+    }
+    
     return (
-        <Card className='mx-2' onClick={() => {navigate(`/workstream/${id}`)}} title={name} subTitle={' Participants: '+ users.length } pt={{ title: { className: "text-sm" }, subTitle: { className: "text-xs" }, body: { className: "m-1" }}}>
+        <>
+        <Toast ref={toast} />
+        <Card className='mx-2' onClick={handleSwitch} title={name} subTitle={' Participants: '+ users.length } pt={{ title: { className: "text-sm" }, subTitle: { className: "text-xs" }, body: { className: "m-1" }}}>
         </Card>
+        </>
     )
 }
 
