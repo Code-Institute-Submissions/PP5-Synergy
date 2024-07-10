fcd1b3c418844a72ad6d36e0d53ce3028e20a4ea
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-07-10 14:22:57 +0100

    Add dropdown option as context

---
+++ frontend/src/contexts/OptionsContext.js
@@ -0,0 +1,35 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+
+export const optionsContext = createContext();
+
+export const useOptions = () => useContext(optionsContext);
+
+export const OptionsContext = ({ children }) => {
+    const [categoryOptions, setCategoryOptions] = useState([])
+    const [projectOptions, setProjectOptions] = useState([])
+
+    const handleMount = async () => {
+        try {
+            const [{ data: category }, { data: project }] =
+            await Promise.all([
+                axiosReq.get(`/api/category/`),
+                axiosReq.get(`/api/project/`),
+            ]);
+            setCategoryOptions(category.results)
+            setProjectOptions(project.results)
+        } catch (err) {
+        console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        handleMount();
+    }, []);
+
+    return (
+        <optionsContext.Provider value={[categoryOptions,projectOptions]}>
+            {children}
+        </optionsContext.Provider>
+    );
+};
