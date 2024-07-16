612b3af29d6928ee46ef4636e8f11f4ba48ffc48
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-07-16 09:52:41 +0100

    Add url for participant id and delete

--- backend/workstream/urls.py
+++ backend/workstream/urls.py
@@ -4,8 +4,11 @@ from workstream import views
 urlpatterns = [
     path('workstream/', views.WorkstreamUserList.as_view()),
     path('workstream/all/', views.WorkstreamList.as_view()),
+    path('workstream/leave/', views.GetLeaveWorkstream.as_view()),
+    path('workstream/leave/<int:pk>/', views.LeaveWorkstream.as_view()),
     path('workstream/active/', views.ParticipantListActive.as_view()),
     path('workstream/participant/', views.ParticipantList.as_view()),
     path('workstream/<int:pk>/', views.WorkstreamDetail.as_view()),
     path('workstream/participant/<int:pk>/', views.ParticipantDetail.as_view()),
+    
 ]
