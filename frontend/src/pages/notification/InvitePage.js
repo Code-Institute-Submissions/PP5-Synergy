4b9449387465ef053b77effe6631626daeec6576
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-07-16 10:20:01 +0100

    Add mount to retrieve workstream list

--- frontend/src/pages/notification/InvitePage.js
+++ frontend/src/pages/notification/InvitePage.js
@@ -1,10 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { axiosReq } from '../../api/axiosDefaults';
 
 const InvitePage = () => {
     const [loaded, setLoaded] = useState(false)
     const [userList, setUserList] = useState({ results: [] });
     const navigate = useNavigate()
+    
+
+    useEffect(() => {
+        const handleMount = async () => {
+          try {
+            const [{ data: userList }] = await Promise.all([
+              axiosReq.get(`/api/workstream/join/`),
+            ]);
+            setUserList(userList)
+          } catch (err) {
+          }
+        };
+        handleMount();
+    }, []);
     
     return (
         <div>
--- frontend/src/pages/notification/JoinPage.js
+++ frontend/src/pages/notification/JoinPage.js
@@ -1,11 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { axiosReq } from '../../api/axiosDefaults';
 
 const JoinPage = () => {
     const [loaded, setLoaded] = useState(false)
     const [workstreamList, setWorkstreamList] = useState({ results: [] });
     const navigate = useNavigate()
     
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+              const [{ data: workstreamList }] = await Promise.all([
+                axiosReq.get(`/api/workstream/join/`),
+              ]);
+              setWorkstreamList(workstreamList)
+              console.log(workstreamList)
+            } catch (err) {
+            }
+          };
+          handleMount();
+    }, []);
+
     return (
         <div>
             join
