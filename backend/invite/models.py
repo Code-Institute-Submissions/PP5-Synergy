23721cf7f4dae85d4cbf2d717326b755358d0824
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-07-17 09:46:52 +0100

    Add related name for user foreign key

--- backend/invite/models.py
+++ backend/invite/models.py
@@ -8,7 +8,7 @@ class Invite(models.Model):
     workstream = models.ForeignKey(Workstream, on_delete=models.CASCADE, related_name='ws_invite')
     created_at = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_invite')
     accepted = models.BooleanField(default=False)
     inbound = models.BooleanField(default=True)
 
