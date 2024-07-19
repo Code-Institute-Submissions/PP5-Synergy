ba5af3b5071e4a3ce1ffd4e03c7c06625bd257b7
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-07-19 09:55:29 +0100

    Add profile component in dashboard

--- frontend/src/pages/auth/Dashboard.js
+++ frontend/src/pages/auth/Dashboard.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Divider } from "primereact/divider";
+import { ScrollPanel } from "primereact/scrollpanel";
+import Profile from '../profile/Profile';
 
 const Dashboard = () => {
   return (
-    <h2>Dashboard</h2>
+    <>
+    <Divider align="right">
+        <div className="inline-flex align-items-center">
+          <i className="pi pi-home mr-2"></i>
+          <b>Dashboard</b>
+        </div>
+      </Divider>
+      <ScrollPanel style={{ width: "100%", height: "40vh" }}>
+        <div className="flex justify-content-start flex-wrap">
+          <Profile />
+        </div>
+      </ScrollPanel>
+    </>
   )
 }
 
